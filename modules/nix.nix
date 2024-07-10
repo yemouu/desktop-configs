518d6bb29d9f6d008a74133b545285f111a00732
yemou <dev@lilac.pink>
2024-07-10 00:14:30 -0400

    Make nix build outside of a tmpfs so builds don't fail due to OOM

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,4 +1,21 @@
-{ ... }: {
+{ pkgs, ... }: {
+  systemd = {
+    services.nix-daemon.environment.TMPDIR = "/nix/tmp";
+    tmpfiles.rules = [ "d /nix/tmp - root root 1d" ];
+  };
+
+  users.users.mou.packages = [
+    (pkgs.symlinkJoin {
+      name = "nixos-rebuild-tmpdir";
+      paths = [ pkgs.nixos-rebuild ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/nixos-rebuild \
+          --set TMPDIR /nix/tmp
+      '';
+    })
+  ];
+
   nix = {
     optimise.automatic = true;
     gc.automatic = true;
