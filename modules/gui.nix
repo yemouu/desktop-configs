7d5e09ba0e3e6e1530d8c0d8272900861bec49e8
yemou <dev@lilac.pink>
2024-09-22 18:06:42 -0400

    Use regreet instead of sddm

--- modules/gui.nix
+++ modules/gui.nix
@@ -3,6 +3,8 @@
 
   environment.pathsToLink = [ "/share/xsessions" "/share/wayland-sessions" ];
 
+  systemd.tmpfiles.rules = [ "C+ /usr/share/wallpapers - - - 1d /home/mou/misc/syncthing/media/images/wallpapers" ];
+
   xdg = {
     mime.enable = true;
     icons.enable = true;
@@ -15,16 +17,28 @@
   services = {
     dbus.enable = true;
     desktopManager.plasma6.enable = true;
-    displayManager.sddm = {
-      wayland.enable = true;
-      enable = true;
-    };
     geoclue2.enable = true;
     xserver.wacom.enable = true;
   };
 
   programs = {
     dconf.enable = true;
+    regreet = {
+      enable = true;
+      cageArgs = [ "-s" "-m" "last" ];
+      font = {
+        package = pkgs.ibm-plex;
+        name = "IBM Plex Sans";
+      };
+      settings.background = {
+        path = "/usr/share/wallpapers/dark/evie-s-eS5hOjf9wwg-unsplash.jpg";
+        fit = "Cover";
+      };
+    };
+    river = {
+      enable = true;
+      extraPackages = with pkgs; [ bemenu swaylock ];
+    };
     xwayland.enable = true;
   };
 
