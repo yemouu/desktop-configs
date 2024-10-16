c0e1c92633878364de87cc69991650e0be1916cf
yemou <dev@lilac.pink>
2024-10-15 23:23:47 -0400

    Add netbird-ui

--- modules/netbird.nix
+++ modules/netbird.nix
@@ -1,4 +1,5 @@
-{ ... }: {
+{ pkgs, ... }: {
   environment.persistence."/data/persistent".directories = [ "/var/lib/netbird" ];
   services.netbird.tunnels.wt0.stateDir = "netbird";
+  users.users.mou.packages = with pkgs; [ netbird-ui ];
 }
