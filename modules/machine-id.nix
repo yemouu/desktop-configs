3350ff6bfa427337bc039df3ed7af8550fe639b4
yemou <dev@lilac.pink>
2024-10-28 06:42:51 -0400

    Correct permissions on /etc/machine-id

--- modules/machine-id.nix
+++ modules/machine-id.nix
@@ -1,4 +1,5 @@
 { config, ... }: {
-  sops.secrets."machine-id" = { };
+  # This needs to be world readable
+  sops.secrets."machine-id".mode = "0444";
   environment.etc."machine-id".source = config.sops.secrets."machine-id".path;
 }
