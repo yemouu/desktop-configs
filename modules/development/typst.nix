40099f83dd2c6664b50163460618d1ba69988ad4
yemou <dev@lilac.pink>
2024-11-05 00:26:43 -0500

    Switch to more recent tools for working with typst and add patch for kakoune to recognize typst files
    typstfmt -> typstyle (typstfmt is being archived)
    typst-lsp -> tinymist (typst-lsp hasn't been seeing much developemnt)
    typst-live -> tinymist (tinymist can also auto compile to pdf)

--- modules/development/typst.nix
+++ modules/development/typst.nix
@@ -1,3 +1,7 @@
 { pkgs, ... }: {
-  users.users.mou.packages = with pkgs; [ typst typstfmt typst-live typst-lsp ];
+  users.users.mou.packages = with pkgs; [
+    tinymist
+    typst
+    typstyle
+  ];
 }
--- modules/editor.nix
+++ modules/editor.nix
@@ -4,7 +4,17 @@
       EDITOR = "kak";
       VISUAL = "kak";
     };
-    systemPackages = with pkgs; [ file kakoune-unwrapped ];
+    systemPackages = with pkgs; [
+      file
+      (kakoune-unwrapped.overrideAttrs (finalAttrs: prevAttrs: {
+        patches = (prevAttrs.patches or [ ]) ++ [
+          (pkgs.fetchpatch {
+            url = "https://patch-diff.githubusercontent.com/raw/mawww/kakoune/pull/5183.patch";
+            hash = "sha256-XEU9pJybAuVuh+/euL6UY+65reXeUUbI2vvI/4Ao9ug=";
+          })
+        ];
+      }))
+    ];
   };
 
   users.users.mou.packages = with pkgs; [ kakoune-lsp ];
